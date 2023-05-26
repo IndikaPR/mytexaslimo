8748655172371558cad94955bc6c606cd282f137
IndikaPR <126867054+IndikaPR@users.noreply.github.com>
2023-05-26 10:49:59 +0530

    making mytexaslimo slider

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-
+const swiper = new Swiper(".section-welcome .swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false,
+  },
+});
